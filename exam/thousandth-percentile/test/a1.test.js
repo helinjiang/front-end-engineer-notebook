1f241892f69fb91d8ee22e2e9a85be4e3c417690
helinjiang <onehlj@qq.com>
2020-12-28 18:24:28 +0800

    feat: 新增 数字转千分位

--- exam/thousandth-percentile/test/a1.test.js
+++ exam/thousandth-percentile/test/a1.test.js
@@ -3,6 +3,18 @@ const { expect } = require('chai');
 
 describe('使用正则零宽断言', function () {
   it('1 -> 1', function () {
-    expect(getResult(1)).to.be.equal('1');
+    expect(getResult(1)).to.equal('1');
+  });
+
+  it('123 -> 123', function () {
+    expect(getResult(123)).to.equal('123');
+  });
+
+  it('1234 -> 1,234', function () {
+    expect(getResult(1234)).to.equal('1,234');
+  });
+
+  it('1234567 -> 1,234,567', function () {
+    expect(getResult(1234567)).to.equal('1,234,567');
   });
 });
